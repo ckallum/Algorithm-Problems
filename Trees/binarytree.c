dfd71818b401fbef49739c7ef67a32341a9bb20f
ckallum <ckallumke@gmail.com>
2019-04-09 08:44:52 +0100

    Hi

--- Trees/binarytree.c
+++ Trees/binarytree.c
@@ -11,7 +11,7 @@ typedef struct node node;
 
 node *newNode(int val){
   node *new = malloc(sizeof(node));
-  (*new) = (node){NULL,NULL, val};
+  (*new) =(node){NULL,NULL,val};
   return new;
 }
 
@@ -22,30 +22,48 @@ void freeTree(node *root){
   free(root);
 }
 
-void insertNode(node *n, int val){
-  if(n->left == NULL){
-    n->left = newNode(val);
+struct node* insertNode(node *n, int val){
+  if (n == NULL) return newNode(val);
+  if(val<n->val){
+    n->left = insertNode(n->left, val);
   }
-  else if(n->right == NULL){
-    n->right = newNode(val);
+  else if(val>n->val){
+    n->right = insertNode(n->right,val);
   }
-  else{
-    if(val<n->left->val){
-      insertNode(n->left, val);
+  return n;
+}
+
+void printinorder(struct node *root)
+{
+    if (root != NULL)
+    {
+        printinorder(root->left);
+        printf("%d \n", root->val);
+        printinorder(root->right);
     }
-    else insertNode(n->right,val);
-  }
 }
 
 int main(){
-  node *root = newNode(10);
+  node *root = NULL;
+  root = insertNode(root, 35);
   insertNode(root, 20);
-  insertNode(root, 30);
+  insertNode(root, 45);
+  insertNode(root, 15);
+  insertNode(root, 25);
   insertNode(root, 40);
   insertNode(root, 50);
-  insertNode(root, 60);
-  assert(root->left->val == 20);
-  // freeTree(root);
-  printf("hello\n");
+  printinorder(root);
+  freeTree(root);
   return 0;
 }
+/*
+             35
+           /    \
+         20     45
+        / \    /  \
+      15  25  40  50
+                    */
+/*The leftmost  branch will always contain the smallest values. All values
+down the left branch of a node will always be smaller than the node which
+is why we do leftmost traversal first when printing and then look at the right
+branches node by node when working back up the tree.  */
